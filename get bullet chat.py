2587c1a5165558fe684beb3a296732843589c079
an-qiyu <134941682+an-qiyu@users.noreply.github.com>
2023-06-02 21:11:16 +0800

    Update get bullet chat.py

--- get bullet chat.py
+++ get bullet chat.py
@@ -14,7 +14,7 @@ def get_date(html_url):
 
 def save(content):#保存弹幕数据
     for i in content:
-        with open('B站弹幕.txt', mode='a', encoding='utf-8') as f:
+        with open('bullet chat.txt', mode='a', encoding='utf-8') as f:
             f.write(i)
             f.write('\n')
             print(i)
