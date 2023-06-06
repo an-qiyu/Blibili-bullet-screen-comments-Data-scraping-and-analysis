1f3f8a301b95cb5bde7ecb46c6b2f73a998b9285
an-qiyu <134941682+an-qiyu@users.noreply.github.com>
2023-06-06 21:44:57 +0800

    Update get bullet chat.py

--- get bullet chat.py
+++ get bullet chat.py
@@ -35,9 +35,9 @@ def save(content):#保存弹幕数据
             print(i)
 
 
-def main(html_url):
-    data = get_date(html_url) #按照日期获得数据进行循环，可直接获得一月的弹幕数据
-    for date in data:
+def main(date_url):
+    mon_data = get_date(date_url) #按照日期获得数据进行循环，可直接获得一月的弹幕数据
+    for date in mon_data:
         url = f'https://api.bilibili.com/x/v2/dm/web/history/seg.so?type=1&oid=185697359&date={date}'  # 弹幕地址
         html_data = get_response(url).text# 字符串形式获得内容
         result = re.findall("[\u4E00-\u9FA5]+", html_data)#获取数据进行正则表达匹配，排除乱码,存为list
@@ -51,3 +51,4 @@ if __name__ == '__main__':
     #下载地址（弹幕日期文件地址）
     date_url = 'https://api.bilibili.com/x/v2/dm/history/index?month=2020-05&type=1&oid=185697359'#日期地址
     main(date_url)
+
